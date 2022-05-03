7d612090d26b6445dbe61acfdc6fa0c8f5ba7941
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-05-03 17:02:28 +0200

    run cms from proxy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { Liquid } from "liquidjs";
+import morgan from "morgan";
+import proxy from "express-http-proxy";
+
+// App constants
+const PORT = 3000;
+const CMS_API_URL = "http://localhost:8055/";
 
 // Create a new express application instance
 const app = express();
-const port = 3000;
 const engine = new Liquid({
   cache: process.env.NODE_ENV === "production",
 });
@@ -12,26 +17,24 @@ const engine = new Liquid({
 app.engine("liquid", engine.express());
 app.set("views", "views");
 app.set("view engine", "liquid");
+
+// Server static files
 app.use(express.static("public"));
 
+// Logger middleware
+app.use(morgan("dev"));
+
 // Define routes
-app.get("/", (_, res) => {
-  res.render("pages/index");
-});
-app.get("/about", (req, res) => {
-  res.render("pages/about");
-});
-app.get("/contact", (req, res) => {
-  res.render("pages/contact");
-});
-app.get("/service", (req, res) => {
-  res.render("pages/service");
-});
-app.get("/blog", (req, res) => {
-  res.render("pages/blog");
-});
+app.get("/", (_, res) => res.render("pages/index"));
+app.get("/about", (_, res) => res.render("pages/about"));
+app.get("/contact", (_, res) => res.render("pages/contact"));
+app.get("/service", (_, res) => res.render("pages/service"));
+app.get("/blog", (_, res) => res.render("pages/blog"));
+
+// Proxy requests to the CMS API
+app.use(proxy(CMS_API_URL));
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
