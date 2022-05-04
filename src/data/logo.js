634226e8e74ba7ca16c1cf656c0a130bf74d96fc
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-05-04 13:55:32 +0200

    logo data

--- src/data/logo.js
+++ src/data/logo.js
@@ -1,10 +1,10 @@
-import { authenticated, directus } from "../controllers/_cms-client";
+import { authenticated, directus } from "../controllers/_cms-client.js";
 
 export default async function getLogoFile() {
   await authenticated();
 
   return directus.files.readByQuery({
-    search: '"name" = "logo.png"',
+    search: "logo.png",
     limit: 1,
   });
 }
