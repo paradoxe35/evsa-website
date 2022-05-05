c04c1d82e52b12e193d2bb0c0bd0af5d5e72d9a0
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-05-05 23:34:01 +0200

    show the rest views

--- src/controllers/about.js
+++ src/controllers/about.js
@@ -1,5 +1,10 @@
-import { directus } from "./_cms-client.js";
+import { CMS_MODELS } from "../data/data.js";
+import { getDatas, hasFiles } from "./_cms-client.js";
 
-export default function about_controller(_, res) {
-  res.render("pages/about");
+export default async function about_controller(_, res) {
+  let { data: welcomeDetails } = await getDatas(CMS_MODELS.welcomeDetails);
+
+  res.render("pages/about", {
+    welcomeDetails: hasFiles(welcomeDetails),
+  });
 }
--- src/controllers/service.js
+++ src/controllers/service.js
@@ -1,5 +1,14 @@
-import { directus } from "./_cms-client.js";
+import { CMS_MODELS } from "../data/data.js";
+import { directus, getDatas, hasFiles } from "./_cms-client.js";
 
-export default function service_controller(_, res) {
-  res.render("pages/service");
+export default async function service_controller(_, res) {
+  let { data: services } = await getDatas(CMS_MODELS.services, undefined, [
+    "date_created",
+  ]);
+  let { data: guards } = await getDatas(CMS_MODELS.guards);
+
+  res.render("pages/service", {
+    services: hasFiles(services),
+    guards: hasFiles(guards),
+  });
 }
